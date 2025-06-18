86e4864112d3518a94c88f0d462ba49b1c89e267
KrSergei <34679066+KrSergei@users.noreply.github.com>
2025-06-18 22:49:25 +0300

    added  generations items pools

--- AP/Assets/Scripts/Objects/ItemSpawner.cs
+++ AP/Assets/Scripts/Objects/ItemSpawner.cs
@@ -6,13 +6,16 @@ public class ItemSpawner : MonoBehaviour, IItemSpawner
     [Header("ITEMS")]
     public List<GameObject> items = new ();
 
+    public int countSpawn = 3;
+
     public Transform spawnSpot;
 
     [SerializeField]
-    private Transform _itemsPoolsStorage;    
+    private Transform _itemsPoolsStorage;
 
 
     [Header("POOLS")]
+    public int preloadCountItems = 10;
     [SerializeField]
     private ItemsPools ItemsPools;
 
@@ -20,14 +23,28 @@ public class ItemSpawner : MonoBehaviour, IItemSpawner
 
     private void Start()
     {
-        for (int i = 0; items.Count > i; i++) 
-        {
-            //SetItem(items[i]);
-            GameObject pool = Instantiate(gameObject, _itemsPoolsStorage);
-            pool.name = items[i].name + "Pool";
-        }
+        InitPools();
     }
 
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+            ItemsPools.GetItem(GetRandom(), spawnSpot, countSpawn);
+    }
+
+    private int GetRandom()
+    {
+        return Random.Range(0, items.Count);
+    }
+
+    private void InitPools()
+    {
+        for (int i = 0; items.Count > i; i++)
+        {
+            //SetItem(items[i]);
+            ItemsPools.Preload(items[i], _itemsPoolsStorage, preloadCountItems, i);
+        }
+    }
 
     public void Clear()
     {
@@ -41,6 +58,6 @@ public class ItemSpawner : MonoBehaviour, IItemSpawner
 
     public void SetItem(GameObject item)
     {
-        ItemsPools.Preload(item, _itemsPoolsStorage.transform);
+        throw new System.NotImplementedException();
     }
 }
--- AP/Assets/Scripts/Objects/ItemsPools.cs
+++ AP/Assets/Scripts/Objects/ItemsPools.cs
@@ -1,10 +1,41 @@
+using Cysharp.Threading.Tasks;
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ItemsPools : MonoBehaviour
 {
-    public void Preload(GameObject item, Transform storage)
+    [SerializeField]
+    private Dictionary<int, Queue<GameObject>> _pools = new ();
+
+    [SerializeField] private float _timeBetweenSpawnItems = 2f;
+
+    public void Preload(GameObject item, Transform storage, int count, int indexPool)
+    { 
+        var pool = new GameObject(item.name);
+
+        Queue<GameObject> items = new ();
+
+        pool.transform.parent = storage;
+
+        for (int i = 0; i < count; i++) 
+        {
+            var currentItem = Instantiate(item, pool.transform);
+            items.Enqueue(currentItem);
+            currentItem.SetActive(false);
+        }
+        _pools.Add(indexPool, items);
+    }
+
+    public async void GetItem(int itemIndex, Transform spawnSpot, int count)
     {
-        GameObject pool = Instantiate(gameObject, storage);
-        pool.name = item.name + "Pool";
+        for (int i = 1; i < count; i++)
+        {
+            _pools.TryGetValue(itemIndex, out var pool);
+            var item = pool.Dequeue();
+            item.transform.position = spawnSpot.position;
+            item.SetActive(true);
+            await UniTask.Delay(TimeSpan.FromSeconds(_timeBetweenSpawnItems));
+        }
     }
 }
